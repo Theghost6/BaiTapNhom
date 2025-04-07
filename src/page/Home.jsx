4c29948c690443c9c44271e690ebc09132c9870d
Theghost6 <nguyensinh15082004@gmail.com>
2025-04-07 18:54:08 +0700

    v1.2

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from "react";<<<<<<< HEAD
+import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { ArrowRight, Star, MapPin, Calendar, TrendingUp } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom";
 import {
   ArrowRight,
   Star,
