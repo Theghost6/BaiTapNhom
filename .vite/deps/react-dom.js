3ebf645b0a5a66ce470a0874a333ee0b621902a4
Theghost6 <nguyensinh15082004@gmail.com>
2025-07-07 18:42:03 +0700

    bigFix

--- .vite/deps/react-dom.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-3N2PMK3V.js";
-import "./chunk-4X6FFAZQ.js";
-import "./chunk-EWTE5DHJ.js";
-export default require_react_dom();
--- .vite/deps/react_jsx-runtime.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-J2CBX5S7.js";
-import "./chunk-4X6FFAZQ.js";
-import "./chunk-EWTE5DHJ.js";
-export default require_jsx_runtime();
